f760555b90597bbcab2e44a3e0d7503c8a9587ae
Oscar Six <26691547+KiloOscarSix@users.noreply.github.com>
2023-01-14 06:19:53 +0000

    catch obscure errors in counting

--- commands/commands_message/fun/counting.ts
+++ commands/commands_message/fun/counting.ts
@@ -13,21 +13,25 @@ module.exports = {
         if (message.channel.id != server.channels.countingChannel)
             return;
 
-        if (message.member?.id == server.counting.lastUser) {
-            await message.react("❌")
-            message.channel.send(`${message.member} RUINED IT AT **${server.counting.number}**!! Next number is **1**. **You can't count two numbers in a row.**`)
-            server.counting.number = 0
-            return server.save()
-        }
         if (Number(message.content) != server.counting.number + 1) {
-            await message.react("❌")
+            await message.react("❌").catch(console.log)
             message.channel.send(`${message.member} RUINED IT AT **${server.counting.number}**!! Next number is **1**. **Wrong Number.**`)
             server.counting.number = 0
+            server.counting.lastUser = ""
+            return server.save()
+        }
+
+        if (message.member?.id == server.counting.lastUser) {
+            await message.react("❌").catch(console.log)
+            message.channel.send(`${message.member} RUINED IT AT **${server.counting.number}**!! Next number is **1**. **You can't count two numbers in a row.**`)
+            server.counting.number = 0
+            server.counting.lastUser = ""
             return server.save()
         }
 
         server.counting.number += 1
         server.counting.lastUser = message.member.id
-        await Promise.all([message.react("✅"), server.save()])
+        await message.react("✅").catch(console.log)
+        await server.save()
     }
 }
