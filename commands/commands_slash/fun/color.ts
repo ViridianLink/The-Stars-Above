b073cef933f6569af7832c05014a49dd8e0ee051
Oscar Six <26691547+KiloOscarSix@users.noreply.github.com>
2023-01-15 21:49:57 +0000

    remove named colours to simplify command

--- commands/commands_slash/fun/color.ts
+++ commands/commands_slash/fun/color.ts
@@ -1,55 +1,25 @@
 import Discord from "discord.js";
 
-const VALID_COLORS: Record<string, string> = {
-    "DEFAULT": "0x000000",
-    "WHITE": "0xffffff",
-    "AQUA": "0x1abc9c",
-    "GREEN": "0x57f287",
-    "BLUE": "0x3498db",
-    "YELLOW": "0xfee75c",
-    "PURPLE": "0x9b59b6",
-    "LUMINOUS VIVID PINK": "0xe91e63",
-    "FUCHSIA": "0xeb459e",
-    "GOLD": "0xf1c40f",
-    "ORANGE": "0xe67e22",
-    "RED": "0xed4245",
-    "GREY": "0x95a5a6",
-    "NAVY": "0x34495e",
-    "DARK AQUA": "0x11806a",
-    "DARK GREEN": "0x1f8b4c",
-    "DARK BLUE": "0x206694",
-    "DARK PURPLE": "0x71368a",
-    "DARK VIVID PINK": "0xad1457",
-    "DARK GOLD": "0xc27c0e",
-    "DARK ORANGE": "0xa84300",
-    "DARK RED": "0x992d22",
-    "DARK GREY": "0x979c9f",
-    "DARKER GREY": "0x7f8c8d",
-    "LIGHT GREY": "0xbcc0c0",
-    "DARK NAVY": "0x2c3e50",
-    "BLURPLE": "0x5865f2",
-    "GREYPLE": "0x99aab5",
-    "DARK BUT NOT BLACK": "0x2c2f33",
-    "NOT QUITE BLACK": "0x23272a",
-}
-
 async function color_set(interaction: Discord.ChatInputCommandInteraction) {
     if (!interaction.guild || !interaction.member || !(interaction.member instanceof Discord.GuildMember))
         return;
 
-    let colorInput = interaction.options.getString("color", true).toUpperCase()
+    let colorInput = interaction.options.getString("color", true).toUpperCase().replace("#", "")
 
-    let color = Number(colorInput)
-
-    if (colorInput in VALID_COLORS) {
-        color = Number(VALID_COLORS[colorInput])
-    } else if (colorInput == "RANDOM") {
-        colorInput = "0x" + Math.floor(Math.random() * 16777215).toString(16).toUpperCase()
-    } else if (isNaN(color)) {
-        return interaction.reply({content: "Invalid color option", ephemeral: true});
+    let color;
+    if (colorInput == "RANDOM") {
+        colorInput = Math.floor(Math.random() * 16777215).toString(16).toUpperCase()
     }
 
-    colorInput = colorInput.replace("0x", "#")
+    if (!colorInput.startsWith("#")) {
+        colorInput = "#" + colorInput;
+    }
+
+    color = parseInt(colorInput.substring(1), 16);
+
+    if (isNaN(color)) {
+        return interaction.reply({content: "Invalid color option", ephemeral: true});
+    }
 
     const existingRole = interaction.guild.roles.cache.find(r => r.name === colorInput)
     if (existingRole) {
@@ -67,7 +37,6 @@ async function color_set(interaction: Discord.ChatInputCommandInteraction) {
 
     await interaction.member.roles.add(role)
     return interaction.reply({content: "Successfully added role", ephemeral: true})
-
 }
 
 module.exports = {
@@ -76,10 +45,10 @@ module.exports = {
         .setDescription("Change your custom color role")
         .addSubcommand(option =>
             option.setName("set")
-                .setDescription("Changes color to provided hex color code OR valid color name. (Enter \"random\" for a random color)")
+                .setDescription("Changes color to provided hex color code. (Enter \"random\" for a random color)")
                 .addStringOption(option =>
                     option.setName("color")
-                        .setDescription("hex color code OR valid color name. (Enter \"random\" for a random color)")
+                        .setDescription("Hex color code OR \"random\"")
                         .setRequired(true)
                 )),
 
