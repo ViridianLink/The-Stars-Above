e6bd083f21dbd7f6e3bdb6406e260eb9fded3f69
Oscar Six <26691547+KiloOscarSix@users.noreply.github.com>
2023-01-16 16:23:29 +0000

    added leveling command

---
+++ commands/commands_message/fun/leveling.ts
@@ -0,0 +1,29 @@
+import Discord from "discord.js";
+import {getServer} from "../../../models/server";
+import {getUserConfig} from "../../../models/user-config";
+
+module.exports = {
+    command: "leveling",
+    callback: async function (message: Discord.Message) {
+        if (!message.guild || !message.member || message.member.user.bot)
+            return;
+
+        const server = await getServer(message.guild.id)
+
+        if (server.leveling.ignoredChannels.includes(message.channel.id))
+            return;
+
+        const user = await getUserConfig(message.member.id)
+
+        if (message.createdTimestamp - user.leveling.lastMessage < 60000) {
+            return
+        }
+
+        const lowerBound = 15
+        const upperBound = 25
+
+        user.leveling.xp += Math.floor(Math.random() * (upperBound + 1 - lowerBound)) + lowerBound
+        user.leveling.lastMessage = message.createdTimestamp
+        await user.save()
+    }
+}
