364aac9802ee0e8eb6569c2e31e673d77299aa71
Oscar Six <26691547+KiloOscarSix@users.noreply.github.com>
2023-03-20 15:26:58 +0000

    updated bot

--- commands/commands_message/fun/leveling.ts
+++ commands/commands_message/fun/leveling.ts
@@ -2,6 +2,15 @@ import Discord from "discord.js";
 import {getServer} from "../../../models/server";
 import {getUserConfig} from "../../../models/user-config";
 
+const levelRoles: Record<number, string> = {
+    5: "1032427564796493905",
+    10: "1068732048396783626",
+    25: "1068732057926242335",
+    50: "1068732056089145375",
+    75: "1068732053857779732",
+    100: "1068732051538321459"
+}
+
 module.exports = {
     command: "leveling",
     callback: async function (message: Discord.Message) {
@@ -33,8 +42,8 @@ module.exports = {
             message.channel.send(`Congrats! ${message.member} just advanced to level ${user.leveling.level}`)
         }
 
-        if (user.leveling.level >= 5) {
-            await message.member.roles.add("1032427564796493905")
+        if (levelRoles.hasOwnProperty(user.leveling.level)) {
+            await message.member.roles.add(levelRoles[user.leveling.level])
         }
         await user.save()
     }
